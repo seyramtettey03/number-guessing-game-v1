5f117716659a7ec752a52b5d9f12efe87ed5dda8
Seyram <seyram.tettey.2022@student.lhschools.org>
2019-10-25 09:48:59 -0400

    inserting conditional

---
+++ src/com/LickinHeights/Main.java
@@ -0,0 +1,39 @@
+package com.LickinHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("do u want a pizza?");
+        String Answer= keyboard.nextLine();
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
